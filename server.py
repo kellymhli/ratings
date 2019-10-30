2e3fba82f019d1846923599d05fda93b596884b7
Your Name <you@example.com>
2019-10-30 11:58:57 -0700

    Create register_form.html

--- server.py
+++ server.py
@@ -33,6 +33,22 @@ def user_list():
     return render_template("user_list.html", users=users)
 
 
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Render register form."""
+
+
+
+    return render_template("register_form.html")
+
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Process resgistration information."""
+
+    return redirect('/')
+
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
